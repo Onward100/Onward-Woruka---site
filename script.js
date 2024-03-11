4394413ac48b4160cb57e5c4a5bea37de4f4dfd5
Woruka Onward <worukaonward@gmail.com>
2024-03-11 01:51:46 +0100

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//NavBar - Hamburger menu
+const hamburgerMenu = document.querySelector(".fa-bars");
+const navMenu = document.querySelector(".nav-menu-overlay");
+const closeMenu = document.querySelector(".close-menu");
+
+hamburgerMenu.addEventListener("click", () =>{
+  navMenu.style.transform = "translateX(0)";
+})
+
+closeMenu.addEventListener("click", () =>{
+  navMenu.style.transform = "translateX(-2000px)";
+})
+
+
+//Services Hover
+const cardHover = document.querySelectorAll(".content-card");
+
+cardHover.forEach((items) => {
+  items.addEventListener("mouseover", () => {
+    items.style.backgroundColor = "#0a0a2e";parent
+    items.children[0].style.color = "white";
+    items.children[1].style.color = "white";
+    items.children[2].style.color = "white";
+    items.children[3].style.color = "white";
+  });
+});
+cardHover.forEach((items) => {
+  items.addEventListener("mouseleave", () => {
+    items.style.backgroundColor = "white";
+    items.children[0].style.color = "#0a0a2e";
+    items.children[1].style.color = "#0a0a2e";
+    items.children[2].style.color = "#0a0a2e";
+    items.children[3].style.color = "#22bec9";
+  });
+});
